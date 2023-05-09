cb95e6555227a9569fb755ca2cd0b33d683608a2
awedc19 <awedc19@daum.net>
2023-05-09 15:15:35 +0900

    FBV로 게시판 포스트 목록 페이지 만들기

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
+    path('board/', include('board.urls')),
     path("admin/", admin.site.urls),
 ]
