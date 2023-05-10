ac653632d384d2a356aa860c823351904d1502ba
awedc19 <awedc19@daum.net>
2023-05-10 18:25:17 +0900

    포스트 상세 페이지 테스트

--- board/tests.py
+++ board/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from .models import Post
 
+
 # Create your tests here.
 
 class TestView(TestCase):
@@ -49,3 +50,39 @@ class TestView(TestCase):
         self.assertIn(post_02.title, main_area.text)
         # 3.4 '아직 게시물이 없습니다'라는 문구는 더 이상 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+
+
+def test_post_detail(self):
+    # 1.1 게시물이 하나 있다.
+    post_01 = Post.objects.create(
+        title='첫 번째 게시물 입니다.',
+        content='Hello World. We are the World',
+    )
+
+    # 1.2 그 게시물의 url은 '/board/1/'이다.
+    self.assertEqual(post_01.get_absolute_url(), '/board/1/')
+
+    # 2. 첫 번째 게시물의 상세 페이지 테스트
+    # 2.1 첫 번째 게시물의 url로 접근하면 정상적으로 작동한다(status code: 200).
+    response = self.client.get(post_01.get_absolute_url())
+    self.assertEqual(response.status_Code, 200)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # 2.2 게시물 목록 페이지와 똑같은 내미게이션 바가 있다.
+    navbar = soup.nav
+    self.assertIn('Blog', navbar.text)
+    self.assertIn('About Me', navbar.text)
+
+    # 2.3 첫 번째 게시물의 제목이 웹 브라우저 탭 타이틀에 들어있다.
+    self.assertIn(post_01.title, soup.title.text)
+
+    # 2.4 첫 번째 게시물의 제목이 포스트 영역에 있다.
+    main_area = soup.find('div', id='main-area')
+    post_area = main_area.find('div', id='post-area')
+    self.assertIn(post_01.title, post_area.text)
+
+    # 2.5 첫 번째 게시물의 작성자(author)가 게시물 영억에 있다(아직 구현할 수 없음)
+    # 아직 작성 불가
+
+    # 2.6 첫 번째 게시물의 내용(content)이 포스트 영역에 있다.
+    self.assertIn(post_01.content, post_area.text)
